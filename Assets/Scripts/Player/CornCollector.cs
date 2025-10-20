465cd0d16687575990273acbf6f6673a1576dbe3
TheBig25 <benmrfo@gmail.com>
2025-10-20 18:14:48 +0100

    Win/Lose Screens
    + Added win and lose screens.

--- Assets/Scripts/Player/CornCollector.cs
+++ Assets/Scripts/Player/CornCollector.cs
@@ -9,17 +9,20 @@ public class CornCollector : MonoBehaviour
 {
     public int Score; // Player's score
     public int requiredScore = 40; // Score needed to win
-    public float loseTime = 30; // Time limit to win in seconds
+    public float loseTime; // Time limit to win in seconds
     public GameObject winScreen, loseScreen; //Stores Win and Lose Screens
 
     public TextMeshProUGUI scoreText; // Reference to the UI text component to display the score
-
+    public TextMeshProUGUI timeText;
+    public float timeLimit;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Score = 0; //Starting score set to 0
         Time.timeScale = 1f; // Ensure the game is running at normal speed
+        loseScreen.SetActive(false);
+        winScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -34,10 +37,12 @@ public class CornCollector : MonoBehaviour
         if (Time.time >= loseTime && !winScreen.activeSelf)
         {
             loseScreen.SetActive(true);
+            timeText.text = "00:00";
             Time.timeScale = 0f; // Pause the game
         }
 
         scoreText.text = "Score: " + Score;
+        TimeControl();
     }
 
     void OnTriggerEnter2D(Collider2D other) 
@@ -49,6 +54,23 @@ public class CornCollector : MonoBehaviour
         }
 
 
+    }
+
+    void TimeControl()
+    {
+        
+        if (timeLimit > 0)
+        {
+            //Decrease the time limit
+            timeLimit -= Time.deltaTime;
+
+            //Format the time display
+            timeText.text = "Time Remaining: " + timeLimit.ToString();
+            float minutes = Mathf.FloorToInt(timeLimit / 60);
+            float seconds = Mathf.FloorToInt(timeLimit % 60);
+
+            timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
     }
 
 }
